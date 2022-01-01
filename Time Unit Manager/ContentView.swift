a5bfe34309fb2393ef250ba02df56d215493781f
Takayuki Cho <taka.3365@gmail.com>
2022-01-02 02:07:21 +0900

    Initial Commit

---
+++ Time Unit Manager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Time Unit Manager
+//
+//  Created by TC on 1/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Time Unit Manager/Time_Unit_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_Unit_ManagerApp.swift
+//  Time Unit Manager
+//
+//  Created by TC on 1/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Time_Unit_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
