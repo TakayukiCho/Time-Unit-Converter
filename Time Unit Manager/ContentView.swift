5b10183b35ac91d4488a25694e4224c45f0b8688
Takayuki Cho <taka.3365@gmail.com>
2022-01-03 03:40:12 +0900

    push

--- Time Unit Manager/ContentView.swift
+++ Time Unit Manager/ContentView.swift
@@ -8,9 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    enum TimeUnit: String, CaseIterable {
+        case hour = "hour"
+        case minute = "minute"
+        case second = "second"
+        
+        var multiplerToSecond: Int {
+            switch self {
+            case .second: return 1
+            case .minute: return 60
+            case .hour: return 3600
+            }
+        }
+    }
+    
+    @State private var durationInput: Int = 0
+    @State private var numberOfPeople: Int = 2
+    
+    @State private var fromUnit: TimeUnit = .hour
+    @State private var toUnit: TimeUnit = .minute
+    
+    @FocusState private var durationIsFocused: Bool
+    
+    var result: Int {
+        return durationInput * fromUnit.multiplerToSecond / toUnit.multiplerToSecond
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("amount", value: $durationInput, format: .number)
+                        .keyboardType(.numberPad)
+                        .focused($durationIsFocused)
+                } header : {
+                    Text("Input")
+                }
+                Section {
+                    Picker("from", selection: $fromUnit) {
+                        ForEach(TimeUnit.allCases, id: \.rawValue) {
+                            Text("\($0.rawValue)").tag($0)
+                        }
+                    }.pickerStyle(.segmented)
+                } header: {
+                    Text("from")
+                }
+
+                Section {
+                    Picker("to", selection: $toUnit) {
+                        ForEach(TimeUnit.allCases, id: \.rawValue) {
+                            Text("\($0.rawValue)").tag($0)
+                        }
+                    }.pickerStyle(.segmented)
+                } header: {
+                    Text("to")
+                }
+                
+                
+                Section {
+                    Text("\(result)")
+                }
+            }.navigationTitle("Time Unit Manager")
+                .toolbar {
+                    ToolbarItemGroup(placement: .keyboard) {
+                        Button("Done") {
+                            durationIsFocused = false
+                        }
+                }
+            }
+        }
     }
 }
 
